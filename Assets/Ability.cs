4ca2e9a34c5e937ddd75bae19bf5b9fb37236cfe
JustinSingleton <jsingle9@msudenver.edu>
2026-02-26 19:04:06 -0700

    ability system build out starting

--- Assets/Ability.cs
+++ Assets/Ability.cs
@@ -41,7 +41,7 @@ public abstract class Ability
         }
     }
 
-    public virtual void TryUse(ICombatant user)
+    public virtual void TryUse(ICombatant user, ICombatant myTarget)
     {
         UnityEngine.Debug.Log($"Using Ability {AbilityName}");
         if(!CanUse(user)){
@@ -50,8 +50,8 @@ public abstract class Ability
         }
         UnityEngine.Debug.Log($"SUCCESS: {AbilityName} allowed → spending cost");
         SpendCost(user);
-        Execute(user);
+        Execute(user, myTarget);
     }
 
-    protected abstract void Execute(ICombatant user);
+    protected abstract void Execute(ICombatant user, ICombatant myTarget);
 }
